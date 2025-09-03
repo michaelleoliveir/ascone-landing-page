1c33243491120014fb3dddf9ac603f892e9f15b6
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-03 20:05:40 -0300

    feat: update title and add lucide-react dependency; enhance Content and Header components

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -6,7 +6,7 @@ export const Content = () => {
             {/* text */}
             <div className="w-[50%] mt-15">
                 <div className='w-[85%]'>
-                    <p className="text-green-800">TRY IT NOW!</p>
+                    <p className="text-green-800 font-bold">TRY IT NOW!</p>
                     <h1 className="text-8xl mt-5">Change the way you use your <span className="italic">money</span></h1>
                     <p className="text-xl mt-16">From your everyday spending, to learning for your future with savings and investment, Ascone helps you get more from your money</p>
                     <button className="bg-(--color-green) text-white p-3 px-8 rounded-3xl mt-10 cursor-pointer">Get Started Now</button>
@@ -14,7 +14,7 @@ export const Content = () => {
             </div>
 
             {/* picture */}
-            <div className="w-[50%] flex items-center justify-center">
+            <div className="w-[50%] mt-15 flex justify-center">
                 <img src={details} alt="" />
             </div>
         </section>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,28 @@
 import "./Header.css"
 
+import { ChevronRight } from "lucide-react" 
+
 const Header = () => {
     return (
         <div className="fixed top-0 left-0 w-full flex flex-row items-center justify-between bg-white p-4 pt-2 px-[8rem] z-50">
             {/* nav bar */}
             <div className="flex flex-row items-center">
                 <h1 className="mr-[5rem] text-(--color-green) text-3xl font-semibold">Ascone</h1>
-                <div className="flex gap-6">
-                    <a href="#">Features</a>
-                    <a href="#">Accounts</a>
-                    <a href="#">Company</a>
-                    <a href="#">Insight</a>
+                <div className="flex gap-4">
+                    <a id="item" href="#">Features</a>
+                    <a id="item" href="#">Accounts</a>
+                    <a id="item" href="#">Company</a>
+                    <a id="item" href="#">Insight</a>
                 </div>
             </div>
 
             {/* login */}
-            <div>
-                <a href="#">Login</a>
-                <button className="bg-(--color-green) text-white ml-2 p-2 px-6 rounded-3xl cursor-pointer">
+            <div className="flex flex-row items-center">
+                <a className="px-5" href="#">Login</a>
+                <button className="flex flex-row gap-2 bg-(--color-green) text-white ml-2 p-2 px-6 rounded-3xl cursor-pointer">
                     Sign Up
-                </button>
+                    <ChevronRight />
+                </button>   
             </div>
         </div>
 
