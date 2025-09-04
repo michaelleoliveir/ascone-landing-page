07410af15d568669cbbfca76c7fcdb53354d815d
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-04 20:11:26 -0300

    refactor: adjust Values component

--- src/components/Values/Values.jsx
+++ src/components/Values/Values.jsx
@@ -11,7 +11,9 @@ export const Values = () => {
                     <div className="w-[50%]">
                         <h1 id="title" className="max-w-[32rem]">Make your spend, Well-spent</h1>
                     </div>
-                    <p className="w-[50%]">Manages a diversified group of specialized private credit brands with efficient tech-enabled processes</p>
+                    <div className="w-[50%] flex flex-row-reverse items-end">
+                        <p className="max-w-[32rem] items-end justify-end text-xl text-gray-500">Manages a diversified group of specialized private credit brands with efficient tech-enabled processes</p>
+                    </div>
                 </div>
             </div>
 
