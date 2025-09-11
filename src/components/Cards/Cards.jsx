bf039bdeb9c463d3dd248abeac9149aeb5710857
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-11 20:43:01 -0300

    refactor: remove unused imports and improve card component layout

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import { useState, cloneElement } from 'react';
 // eslint-disable-next-line no-unused-vars
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { MoveUpRight } from 'lucide-react';
 import { cardData } from './cardData';
 
@@ -29,25 +29,25 @@ const Cards = () => {
     };
 
     return (
-        <div className='mt-18 flex flex-col md:flex-row justify-center items-center gap-5'>
+        <div className='mt-18 flex md:flex-row justify-center gap-10'>
             {cardData.map((card, index) => (
                 <motion.div
                     key={index}
-                    className={`card cursor-pointer h-[500px] bg-cover bg-center border-2 ${index === expandedIndex ? 'bg-(--color-beige) border-(--color-beige) rounded-tr-[10rem]' : 'bg-white border-gray-200'}`}
+                    className={`card cursor-pointer h-[500px] border-2 ${index === expandedIndex ? 'bg-(--color-beige) border-(--color-beige) rounded-tr-[10rem]' : 'bg-white border-gray-200'}`}
                     variants={cardVariants}
                     initial="collapsed"
                     animate={index === expandedIndex ? 'expanded' : 'collapsed'}
                     onClick={() => handleCardClick(index)}
                 >
-                    <div className="flex flex-col justify-center items-center h-full">
-                        <div className="flex flex-col justify-center items-start h-full px-4 w-[80%] mx-auto">
+                    <div className="flex flex-col h-full">
+                        <div className="flex flex-col justify-center h-full px-4 w-[80%] mx-auto">
                             <div className='mb-13'>
                                 {cloneElement(card.icon, {
                                     color: index === expandedIndex ? '#022f30' : 'black',
                                     size: 52,
                                 })}
                             </div>
-                            <h1 className={`text-2xl font-semibold py-4 text-left w-full ${index === expandedIndex ? 'text-(--color-green)' : 'text-black'}`}>
+                            <h1 className={`text-2xl font-semibold py-4 text-left ${index === expandedIndex ? 'text-(--color-green)' : 'text-black'}`}>
                                 {card.title}
                             </h1>
                             <p className={`text-left text-xl w-full ${index === expandedIndex ? 'text-(--color-green)' : 'text-gray-500'}`}>
